a4be506487fb1fa314a46afdc05c184d1fa85e5c
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-08-23 18:02:40 +0100

    formatted retrieved data

--- app/index.js
+++ app/index.js
@@ -1,8 +1,20 @@
 const fetch = require('node-fetch')
 
 async function getUsers() {
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    let response = await fetch('https://jsonplaceholder.typicode.com/users/1')
     let data = await response.json()
     return data;
 }
-getUsers().then((data) => console.log(data))
+getUsers().then((data) => console.log(`
+User Details:
+
+    Name: ${data.name}
+    Username: ${data.username}
+    Address: ${data.address.suite}, ${data.address.street}, ${data.address.city}, ${data.address.zipcode}
+    Contact: ${data.phone} or ${data.email}
+
+Company Details: 
+
+    Name: ${data.company.name}
+    Website: ${data.website}
+`))
